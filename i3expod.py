b762db8ffcc77d8e42af9670752f9d351341a368
Massimo Girondi <massimo@girondi.net>
2021-02-25 19:22:25 +0100

    Merged utils back to i3expod

--- i3expod.py
+++ i3expod.py
@@ -23,7 +23,25 @@ from PIL import Image, ImageDraw
 from xdg.BaseDirectory import xdg_config_home
 from contextlib import suppress
 from PIL import Image, ImageFilter, ImageEnhance
-from utils import *
+
+def get_primary_output_name():
+    stdout,stderr = subprocess.Popen('xrandr --listmonitors',
+                    shell=True, stdout=subprocess.PIPE).communicate()
+    if stdout != '':
+        monitorlines = stdout.decode().split("\n")
+        # Search for the primary (marked with +*)
+        # If none found (e.g. primary is on a disconnected output), take the first
+        primary=None
+        for m in monitorlines:
+          if "+*" in m:
+            primary=m
+            break # Early exit from the cycle
+          elif "+" in m:
+            primary=m   # We found a monitor. Keep it
+        if primary != None:
+            return primary.split()[-1]
+    return None
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--fullscreen", action="store_true",
